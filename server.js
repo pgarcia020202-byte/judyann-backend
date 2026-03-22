067fb1e20388375e7eb061ef973eadb6843a4073
Judy Ann Tatunay <jatatunay7613@student.dmmmsu.edu.ph>
2026-03-22 09:43:48 +0800

    Add Vercel frontend URL to CORS

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const allowedLocalhostOrigins = new Set([
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'http://localhost:3001',
-  'http://127.0.0.1:3001'
+  'http://127.0.0.1:3001',
+  'https://judyann-frontend.vercel.app'
 ]);
 
 app.use(
